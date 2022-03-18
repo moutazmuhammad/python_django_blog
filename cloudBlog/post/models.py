4b3a42985d900ed7cb08fae3735f91c9e0e6d11f
Moutaz <moutazmuhammad1997@gmail.com>
2022-03-18 18:53:34 +0200

    Add comments to posts

--- cloudBlog/post/models.py
+++ cloudBlog/post/models.py
@@ -1,7 +1,9 @@
+from ctypes.wintypes import POINT
 from curses import meta
 from distutils.command.upload import upload
 from operator import mod
 from pyexpat import model
+from turtle import pos
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
@@ -18,7 +20,26 @@ class Post(models.Model):
     class Meta:
         ordering= ('-created',)
 
+    def comment_count(self):
+        return self.comment_set.all().count()
+
+    def comments(self):
+        return self.comment_set.all()
+
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.CharField(max_length=300)
+    comment_data = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering= ('-comment_data',)
+
+    def __str__(self):
+        return self.body
+
 
 
